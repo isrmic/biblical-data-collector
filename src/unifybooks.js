a9d15164bb849707fc82363cbcadf3e016ff0a58
isrmic <israelfcfm@outlook.com>
2019-03-04 19:47:29 -0300

    modified method to unify books in a one file json (nvt.json)

--- src/unifybooks.js
+++ src/unifybooks.js
@@ -3,17 +3,25 @@ const {
   join
 } = require('path');
 
+const listorderedbooks = ["Gênesis","Êxodo","Levítico","Números","Deuteronômio","Josué","Juízes","Rute","1Samuel","2Samuel","1Reis","2Reis","1Crônicas","2Crônicas","Esdras","Neemias","Ester","Jó","Salmos","Provérbios","Eclesiastes","Cântico dos Cânticos","Isaías","Jeremias","Lamentações","Ezequiel","Daniel","Oseias","Joel","Amós","Obadias","Jonas","Miqueias","Naum","Habacuque","Sofonias","Ageu","Zacarias","Malaquias", "Mateus","Marcos","Lucas","João","Atos","Romanos","1Coríntios","2Coríntios","Gálatas","Efésios","Filipenses","Colossenses","1Tessalonicenses","2Tessalonicenses","1Timóteo","2Timóteo","Tito","Filemom","Hebreus","Tiago","1Pedro","2Pedro","1João","2João","3João","Judas","Apocalipse"];
+
 const config = require('../config');
 
 const booksdir = config.book.directory;
 
-const filesbooksdir = fs.readdirSync(booksdir);
+// const filesbooksdir = fs.readdirSync(booksdir);
 
 const books = [];
 
-filesbooksdir.map(filename => {
+// filesbooksdir.map(filename => {
 
-  const bookcontent = fs.readFileSync(join(booksdir, filename), 'utf8').trim();
+//   const bookcontent = fs.readFileSync(join(booksdir, filename), 'utf8').trim();
+//   const book = JSON.parse(bookcontent);
+//   books.push(book);
+// });
+
+listorderedbooks.forEach(bookname => {
+  const bookcontent = fs.readFileSync(join(booksdir, bookname + '.json'), 'utf8').trim();
   const book = JSON.parse(bookcontent);
   books.push(book);
 });
